35b6bb50a613cc86f0fb9ce9e94c199032d51f21
inayyah04 <innayahazizah00@gmail.com>
2025-06-19 13:09:41 +0700

    done

---
+++ app/src/main/java/com/example/app_leaff/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.app_leaff
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val splashTime: Long = 2000 // 2 detik
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTime)
+    }
+}
